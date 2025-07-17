06197484ca1a62667953eef3ccc7af83a7f35243
balqis17project <a.blqss.1725@gmail.com>
2025-07-17 17:15:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,17 @@ import availabilityRoutes from './routes/availability.js';
 dotenv.config();
 
 const app = express();
+const express = require('express');
+const cors = require('cors');
+
 app.use(cors());
 app.use(express.json());
 app.use('/api/availability', availabilityRoutes);
+const authRoutes = require('./routes/auth'); // pastikan path betul
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://quickride_user:anang6428@clusterbalqis.tddcfxr.mongodb.net/?retryWrites=true&w=majority&appName=ClusterBalqis')
   .then(() => {
